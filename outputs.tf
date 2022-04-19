53bcab669a0dd34206c41ff012de3ee33693844f
astorozhevsky <astorozhevsky@ronasit.com>
2022-04-19 19:16:40 +0600

    feat: added output ingress ip address

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,7 @@ output "ci_private_key" {
   value     = google_service_account_key.ci_key.private_key
   sensitive = true
 }
+
+output "ingress_ip_address" {
+  value = google_compute_address.ingress_ip_address.address
+}
