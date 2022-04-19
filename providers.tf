b3c77ae71d8b7a6eee1ac8a2cfb72ad2e84074c3
astorozhevsky <astorozhevsky@ronasit.com>
2022-04-19 19:20:39 +0600

    fix: incorrect cluster_ca_certificate

--- providers.tf
+++ providers.tf
@@ -3,13 +3,13 @@ data "google_client_config" "default" {}
 provider "kubernetes" {
   host                   = "https://${module.gke.endpoint}"
   token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(module.project-factory.gke.ca_certificate)
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
 }
 
 provider "helm" {
   kubernetes {
     host                   = "https://${module.gke.endpoint}"
     token                  = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(module.project-factory.gke.ca_certificate)
+    cluster_ca_certificate = base64decode(module.gke.ca_certificate)
   }
 }
