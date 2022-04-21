6791d97faf6e9e7e9c6bc0a272c484dfd097ade3
astorozhevsky <astorozhevsky@ronasit.com>
2022-04-21 11:13:08 +0600

    feat(outputs): added cluster_region

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,11 @@ output "cluster_name" {
   description = "The name of the cluster"
 }
 
+output "cluster_region" {
+  value       = module.gke.region
+  description = "The region of the cluster"
+}
+
 output "ci_private_key" {
   value       = google_service_account_key.ci_key.private_key
   description = "The private key of the CI service account"
